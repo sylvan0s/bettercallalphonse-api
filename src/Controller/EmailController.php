2aa007adf632a561e678a3672ca988c27e28b342
root <root@debian>
2018-06-12 13:38:42 +0200

    Email is working now

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -10,29 +10,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class EmailController extends Controller
 {
     /**
-     * @Route("/send_register_email", name="send_register_email")
+     * @Route("/send_register_email", name="send_register_email", methods={"POST"})
      */
     public function index(Request $request, \Swift_Mailer $mailer)
     {
-      $datas = $request->request->all();
+      $datas = json_decode($request->getContent());
       // var_dump($datas); die;
 
       $register_email = (new \Swift_Message('Bienvenue sur Better Call Altimate !'))
                         ->setFrom('jlgd@altimate.pro')
-                        ->setTo($datas['email'])
+                        ->setTo($datas->email)
                         ->setBody(
                           $this->renderView(
                             'email/index.html.twig',
                             array(
-                              'username' => $datas['username'],
-                              'password' => $datas['password']
+                              'username' => $datas->username,
+                              'password' => $datas->password
                             )
                           ),
                           'text/html'
                         );
 
       $mailer->send($register_email);
-      
+
       return new Response('Email envoyé', Response::HTTP_OK, array('content-type', 'application/json'));
       // return $this->render('email/index.html.twig', [
       //     'controller_name' => 'EmailController',
