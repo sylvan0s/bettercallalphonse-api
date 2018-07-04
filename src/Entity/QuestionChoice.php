1471841c02fe8d79d6668ad8234c7a178a028b6f
sylvan0s <sylvan0s@github.com>
2018-07-04 11:45:58 +0200

    fixing result issue on question

--- src/Entity/QuestionChoice.php
+++ src/Entity/QuestionChoice.php
@@ -61,14 +61,14 @@ class QuestionChoice
     /**
      * @ORM\Column(type="string", length=255, nullable=false)
      * @Groups({"questionRead", "question_choiceRead", "question_choiceWrite", "user_question_choiceRead",
-     *     "collabs_user_question_choiceRead"})
+     *     "collabs_user_question_choiceRead", "my_user_question_choiceRead"})
      */
     private $libelle;
 
     /**
      * @ORM\Column(type="integer", nullable=false)
      * @Groups({"questionRead", "question_choiceRead", "question_choiceWrite", "user_question_choiceRead",
-     *     "collabs_user_question_choiceRead"})
+     *     "collabs_user_question_choiceRead", "my_user_question_choiceRead"})
      */
     private $note;
 
